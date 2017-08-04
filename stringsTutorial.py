ba1b33e820bc943336a5dd549d57b44634632f66
Adam Clark <Autoboticon@gmail.com>
2017-08-04 10:45:00 +0100

    Strings tutorial

---
+++ stringsTutorial.py
@@ -0,0 +1,45 @@
+print('spam eggs')
+print('dosen\'t')
+print("doesn't")
+print('"Yes," he said.')
+print("\"Yes,\" he said.")
+print('"Isn\'t," she said.')
+output = 'First line.\nSecond line.'
+print(output)
+print('C:\some\name')
+print(r'C:\some\name')
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+print(3*'un'+'ium')
+print('Py''thon')
+prefix = 'Py'
+print(prefix + 'thon')
+text = ('Put several strings with smooth boys '
+        'to have them joined togethor.')
+print(text)
+word = 'Python'
+print(word[0])
+print(word[5])
+print(word[-1])
+print(word[-2])
+print(word[-6])
+print(word[0:2])
+print(word[2:5])
+print(word[:2] + word[2:])
+print(word[:4] + word[4:])
+print(word[:2])
+print(word[4:])
+print(word[-2:])
+print(word[4:42])
+print(word[42:])
+print('J' + word[1:])
+print(word[:2] + 'py')
+longWord = 'supercalifragilisticexpialidocious'
+print(len(longWord))
+#putting it all togethor
+if(len(longWord) >= 34):
+    print('That'' is a long word '
+          'you got there pal!')
