94377946402eac3016db9a87c615f7876133bdb0
Adam Clark <Autoboticon@gmail.com>
2017-08-04 10:14:50 +0100

    Maths Tutorial

---
+++ mathsTutorial.py
@@ -0,0 +1,24 @@
+print(2+2)
+print(50 - 5*6)
+print((50-5*6)/4)
+print(8/5) 
+print(17/3)
+print(17//3)
+print(17%3)
+print(5*3+2)
+print(5**2)
+print(2**8)
+width = 20
+height = 5*9
+print(width*height)
+print(4*3.75-1)
+tax = 20/100
+price = 100.50
+total = price*tax
+print(total)
+print(price+total)
+print(round((price+total),2))
+#putting it all togeathor
+if(total + price > 100):
+    print("That's a lot...")
+    
