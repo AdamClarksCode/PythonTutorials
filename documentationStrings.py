281d64e911af88813056af63974adeb3c38dab97
Adam Clark <Autoboticon@gmail.com>
2017-08-07 07:42:07 +0100

    Tutorials over weekend

---
+++ documentationStrings.py
@@ -0,0 +1,8 @@
+def myFunction():
+    """Do nothing, but document it.
+
+    No, really, it doesn't do anything.
+    """
+    pass
+
+print(myFunction.__doc__)
---
+++ lamdbaExpressions.py
@@ -0,0 +1,9 @@
+def makeIncrementor(n):
+    return lambda x: x+n
+f = makeIncrementor(42)
+print(f(0))
+print(f(1))
+pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+pairs.sort(key=lambda pair: pair[1])
+print(pairs)
+
